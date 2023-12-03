3f6807df14b8cfbf34d427b99fc2b7d547a50fef
he ting <2975587905@qq.com>
2023-12-03 16:10:43 +0800

    init syntax analysis

---
+++ Syntax analysis/src/antlr/clong.g4
@@ -0,0 +1,68 @@
+//antlr4 -v 4.7.2 -Dlanguage=Python3 clong.g4 -visitor
+grammar clong;
+
+prog:head main EOF;
+//导入语句：忽略
+head:(include)*;
+include:'#include' '<' .*? ('.h'|'.cpp')? '>';
+//main语句
+main:('int'|'void') 'main''()''{' body '}';
+//main内语句
+body:(func|block)*;
+//函数
+func:(printf|gets|strlen|returnFunc)';';
+//语句块
+block:ifElse|whileFunc|forFunc|initParam|initArray|assign;
+//printf函数
+printf:'printf''('((stringm(','exp)*) |paramName)')';
+//gets函数
+gets:'gets''('paramName')';
+//strlen函数
+strlen:'strlen''('paramName')';
+//return函数
+returnFunc:'return'(intm)?;
+//if-else语句
+ifElse:ifFunc(elseif)*(elseFunc)?;
+ifFunc:'if''('exp')''{'body'}';
+elseif:'else''if''('exp')''{'body'}';
+elseFunc:'else''{'body'}';
+//while语句
+whileFunc:'while''('exp')''{'body'}';
+//for语句
+forFunc:'for''('initFunc';'exp';'assignFunc')''{'body'}';
+initFunc: (typeParam)? paramName ('=' exp)?(','initFunc)?|;
+assignFunc:paramName '=' exp(','assignFunc)?|;
+//初始化参数
+initParam:typeParam paramName ('=' exp)?(','paramName ('=' exp)?)*';';
+//初始化数组
+initArray:typeParam paramName '[' intm ']'';';
+//参数,数组参数赋值
+assign:((paramName|array)'='exp)+exp';';
+//表达式
+exp:charm|stringm|paramName|(op='-')?(intm|doublem)|array|func|'('exp')'
+    |exp op=('+'|'-'|'*'|'/'|'%'|'>'|'<'|'>='|'<='|'=='|'!='|'&&'|'||') exp
+    |op='!'exp;
+//类型
+typeParam:'int'|'char'|'double';
+//数组元素
+array:paramName'['intm']';
+//变量名
+paramName:PARAM;
+PARAM: [a-zA-Z_][0-9A-Za-z_]*;
+//int值
+intm:INT;
+INT: [0-9]+;
+//double值
+doublem:DOUBLE;
+DOUBLE: [0-9]+'.'[0-9]+;
+//字符
+charm:CHAR;
+CHAR: '\''.'\'';
+//字符串
+stringm:STRING;
+STRING: '"'.*?'"';
+//跳过符号
+LineComment: '//'.*?'\r'?'\n'   -> skip;
+BlockComment:  '/*'.*?'*/'  -> skip;
+DELIMITER: [\t\r\n]+ -> skip ;
+SPACE:[ ]+ -> skip;
---
+++ Syntax analysis/src/antlr/clongVisitor.py
@@ -0,0 +1,163 @@
+# Generated from clong.g4 by ANTLR 4.7.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .clongParser import clongParser
+else:
+    from clongParser import clongParser
+
+# This class defines a complete generic visitor for a parse tree produced by clongParser.
+
+class clongVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by clongParser#prog.
+    def visitProg(self, ctx:clongParser.ProgContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#head.
+    def visitHead(self, ctx:clongParser.HeadContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#include.
+    def visitInclude(self, ctx:clongParser.IncludeContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#main.
+    def visitMain(self, ctx:clongParser.MainContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#body.
+    def visitBody(self, ctx:clongParser.BodyContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#func.
+    def visitFunc(self, ctx:clongParser.FuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#block.
+    def visitBlock(self, ctx:clongParser.BlockContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#printf.
+    def visitPrintf(self, ctx:clongParser.PrintfContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#gets.
+    def visitGets(self, ctx:clongParser.GetsContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#strlen.
+    def visitStrlen(self, ctx:clongParser.StrlenContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#returnFunc.
+    def visitReturnFunc(self, ctx:clongParser.ReturnFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#ifElse.
+    def visitIfElse(self, ctx:clongParser.IfElseContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#ifFunc.
+    def visitIfFunc(self, ctx:clongParser.IfFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#elseif.
+    def visitElseif(self, ctx:clongParser.ElseifContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#elseFunc.
+    def visitElseFunc(self, ctx:clongParser.ElseFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#whileFunc.
+    def visitWhileFunc(self, ctx:clongParser.WhileFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#forFunc.
+    def visitForFunc(self, ctx:clongParser.ForFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#initFunc.
+    def visitInitFunc(self, ctx:clongParser.InitFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#assignFunc.
+    def visitAssignFunc(self, ctx:clongParser.AssignFuncContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#initParam.
+    def visitInitParam(self, ctx:clongParser.InitParamContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#initArray.
+    def visitInitArray(self, ctx:clongParser.InitArrayContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#assign.
+    def visitAssign(self, ctx:clongParser.AssignContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#exp.
+    def visitExp(self, ctx:clongParser.ExpContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#typeParam.
+    def visitTypeParam(self, ctx:clongParser.TypeParamContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#array.
+    def visitArray(self, ctx:clongParser.ArrayContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#paramName.
+    def visitParamName(self, ctx:clongParser.ParamNameContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#intm.
+    def visitIntm(self, ctx:clongParser.IntmContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#doublem.
+    def visitDoublem(self, ctx:clongParser.DoublemContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#charm.
+    def visitCharm(self, ctx:clongParser.CharmContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by clongParser#stringm.
+    def visitStringm(self, ctx:clongParser.StringmContext):
+        return self.visitChildren(ctx)
+
+
+
+del clongParser
